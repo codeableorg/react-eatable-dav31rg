8589eb5c0d5d21b9fc3a43c5dedfb410a39b6bf9
Davel <davel.reymundo@gmail.com>
2023-03-30 13:03:51 -0500

    Add routes to the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
-import DishesList from './pages/DishesList'
 import { DishesProvider } from './context/DishesContext'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import DishesList from './pages/DishesList'
+import DishDetail from './pages/DishDetail'
+import EditDish from './pages/EditDish'
+import CreateDish from './pages/CreateDish'
 
 function App() {
 
 
   return (
     <DishesProvider>
-      <DishesList />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<DishesList />} />
+          <Route path='/dish' element={< DishDetail />} />
+          <Route path='/create' element={< CreateDish />} />
+          <Route path='/edit' element={< EditDish />} />
+        </Routes>
+      </BrowserRouter>
     </DishesProvider>
   )
 }
