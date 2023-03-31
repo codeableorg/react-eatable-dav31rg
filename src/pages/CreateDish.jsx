6350aeea5620814faf9bb605939641e9ca1f7e79
Davel <davel.reymundo@gmail.com>
2023-03-30 23:27:08 -0500

    Add Create new dish page

--- src/pages/CreateDish.jsx
+++ src/pages/CreateDish.jsx
@@ -1,8 +1,22 @@
+import styled from '@emotion/styled';
 import React from 'react'
+import DishForm from '../components/Form'
+
+const StyledTitle = styled.p`
+  font-weight: 600;
+  font-size: 1.4em;
+  line-height: 1.8em;
+  text-align: center; 
+  margin-top: 3em;
+  margin-bottom: 3em;
+`;
 
 const CreateDish = () => {
   return (
-    <div>CreateDish Form</div>
+    <>
+      <StyledTitle>Create Product</StyledTitle>
+      <DishForm buttonName="Create"/>
+    </>
   )
 }
 
